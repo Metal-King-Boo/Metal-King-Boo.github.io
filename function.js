eb76fa4f086c73baf9225c51047b23a04801c073
metal-king-boo <faizu901@yahoo.com>
2024-06-20 17:18:11 -0400

    added side nav bar

---
+++ function.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.body.style.backgroundColor = "black";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "indigo";
+}
