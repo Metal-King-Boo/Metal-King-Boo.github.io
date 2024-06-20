78b933e9693afae3c30f4d8164e5a811781808d9
metal-king-boo <faizu901@yahoo.com>
2024-06-20 17:23:06 -0400

    Update function.js

--- function.js
+++ function.js
@@ -7,3 +7,5 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "indigo";
 }
+
+/* https://www.w3schools.com/howto/howto_css_sidenav_buttons.asp */
